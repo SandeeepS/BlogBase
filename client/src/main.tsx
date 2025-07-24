b7430fbaaec0bd4198270c0726bf7d93ffcb6de0
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-07-24 11:15:37 +0530

    strict mode commented

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
@@ -10,11 +10,11 @@ import { PersistGate } from "redux-persist/integration/react";
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
-      <StrictMode>
+      {/* <StrictMode> */}
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
-      </StrictMode>
+      {/* </StrictMode> */}
     </Provider>
   </BrowserRouter>
 );
